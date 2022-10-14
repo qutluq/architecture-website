5500954b8fef9eda2fa7ff45e7f056a2885cab9d
Qutluq <kootlook.applications@gmail.com>
2022-10-14 22:44:12 +0600

    feat: navigation map

---
+++ src/config/site.tsx
@@ -0,0 +1,7 @@
+export const navigation = [
+  { name: 'main', pathName: '/' },
+  { name: 'gallery', pathName: '/gallery' },
+  { name: 'projects', pathName: '/projects' },
+  { name: 'certifications', pathName: '/certifications' },
+  { name: 'contacts', pathName: '/contacts' }
+]
