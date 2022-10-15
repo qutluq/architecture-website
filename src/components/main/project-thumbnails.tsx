1eba1ed63985b10fdd6b81c3524a84724f0a06d2
Qutluq <kootlook.applications@gmail.com>
2022-10-15 10:10:54 +0600

    style: made buttons clickable

--- src/components/main/project-thumbnails.tsx
+++ src/components/main/project-thumbnails.tsx
@@ -3,6 +3,7 @@ import {
   ArrowLongRightIcon,
   ArrowLongLeftIcon,
 } from "@heroicons/react/24/solid";
+import { Button } from "src/components/button";
 
 export const ProjectThumbnails = () => (
   <div className=" flex flex-col items-center justify-between lg:flex-row">
@@ -12,8 +13,18 @@ export const ProjectThumbnails = () => (
         <span className="font-medium capitalize text-gray-700">Lorum</span>
       </div>
       <div className="hidden gap-10 lg:flex">
-        <ArrowLongLeftIcon className="inline w-6" aria-hidden="true" />
-        <ArrowLongRightIcon className="inline w-6" aria-hidden="true" />
+        <Button variant="text" className="border-0">
+          <ArrowLongLeftIcon
+            className="inline w-6 text-gray-700"
+            aria-hidden="true"
+          />
+        </Button>
+        <Button variant="text" className="border-0">
+          <ArrowLongRightIcon
+            className="inline w-6 text-gray-700"
+            aria-hidden="true"
+          />
+        </Button>
       </div>
       <div className="hidden gap-3 lg:flex">
         <span>1</span>
@@ -25,10 +36,17 @@ export const ProjectThumbnails = () => (
       <div className="items-baseline lg:ml-10">
         <Image width={770} height={829} src="/assets/building-image.png" />
       </div>
-      <div className="w-50 absolute bottom-0 left-0 z-50 flex items-center bg-white p-3 lg:ml-10">
-        <span className="uppercase">view project</span>&nbsp;&nbsp;&nbsp;
-        <ArrowLongRightIcon className="inline w-6" aria-hidden="true" />
-      </div>
+      <Button
+        variant="text"
+        className="w-50 absolute bottom-0 left-0 z-50 flex items-center border-0 bg-white p-3 lg:ml-10"
+      >
+        <span className="uppercase text-gray-700">view project</span>
+        &nbsp;&nbsp;&nbsp;
+        <ArrowLongRightIcon
+          className="inline w-6 text-gray-700"
+          aria-hidden="true"
+        />
+      </Button>
     </div>
     <div className="mt-5 flex gap-10 lg:hidden">
       <ArrowLongLeftIcon className="inline w-6" aria-hidden="true" />
