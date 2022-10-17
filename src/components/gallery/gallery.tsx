c5e229cdd2b4232050e67395435dc8b62b4e927e
Qutluq <kootlook.applications@gmail.com>
2022-10-17 16:37:18 +0600

    refactoring: changed gap size & code style

--- src/components/gallery/gallery.tsx
+++ src/components/gallery/gallery.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 
 type PropTypes = {
-  images: string[]
-}
+  images: string[];
+};
 
-export const Gallery = ({images}: PropTypes) => {
+export const Gallery = ({ images }: PropTypes) => {
   return (
     <div className="relative mx-auto mb-20 flex min-h-full w-full flex-col items-center justify-center gap-3 px-10 lg:gap-10">
       <div className="flex w-full items-center justify-start xl:justify-center">
-        <div className="flex flex-col justify-center gap-0 py-5 text-xl uppercase text-gray-400 lg:gap-3 lg:text-7xl xs:text-3xl">
+        <div className="flex flex-col justify-center gap-0 py-5 text-xl uppercase text-gray-400 lg:gap-1 lg:text-7xl xs:text-3xl">
           Photo
           <span className="font-medium capitalize text-gray-700">Gallery</span>
         </div>
